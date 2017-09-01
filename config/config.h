9de87ddbb72c6d6440b424ba3d26fdc3987cb190
Richard Russon <rich@flatcap.org>
2017-09-01 18:15:26 +0100

    doxy glue

---
+++ config/config.h
@@ -0,0 +1,60 @@
+/**
+ * @file
+ * Convenience wrapper for the config headers
+ *
+ * @authors
+ * Copyright (C) 2017 Richard Russon <rich@flatcap.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @page config Flexible handling of config items
+ *
+ * User configurable variables.
+ *
+ * -# @subpage account
+ * -# @subpage address
+ * -# @subpage bool
+ * -# @subpage magic
+ * -# @subpage mbtable
+ * -# @subpage number
+ * -# @subpage path
+ * -# @subpage quad
+ * -# @subpage regex
+ * -# @subpage set
+ * -# @subpage sort
+ * -# @subpage string
+ */
+
+#ifndef _CONFIG_CONFIG_H
+#define _CONFIG_CONFIG_H
+
+#include "account.h"
+#include "address.h"
+#include "bool.h"
+#include "inheritance.h"
+#include "magic.h"
+#include "mbtable.h"
+#include "number.h"
+#include "path.h"
+#include "quad.h"
+#include "regex2.h"
+#include "set.h"
+#include "sort.h"
+#include "string3.h"
+#include "types.h"
+
+#endif /* _CONFIG_CONFIG_H */
